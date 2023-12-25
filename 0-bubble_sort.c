35f05beec39e4b1cb458972c2a64b1ad09166884
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-25 00:42:11 +0000

    ---

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,32 +11,17 @@ void bubble_sort(int *arr, size_t size)
 	size_t i, j, temp;
 	bool swaped;
 
-	do
-	{
-		swaped = false;
-		for (i = 0 ; i < size ; i++)
-			for (j = 0 ; j < size - 1 - i ; j++)
-				if (arr[j] > arr[j + 1])
-				{
-					swaped = true;
-					temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-					print_array(arr, size);
-				}
+	do {
+	swaped = false;
+	for (i = 0 ; i < size ; i++)
+		for (j = 0 ; j < size - 1 - i ; j++)
+			if (arr[j] > arr[j + 1])
+			{
+				swaped = true;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				print_array(arr, size);
+			}
 	} while (swaped);
-}
-
-int main(void)
-{
-	int arr[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(arr) / sizeof(arr[0]);
-
-	print_array(arr, n);
-	printf("\n");
-	bubble_sort(arr, n);
-	printf("\n");
-	print_array(arr, n);
-
-	return (0);
 }
--- printing_func.c
+++ printing_func.c
@@ -1,25 +1,5 @@
 #include "sort.h"
 
-/**
- * print_array - print array element
- * @arr: array
- * @size: size_t
- * Return: None
- */
-
-void print_array(const int *arr, size_t size)
-{
-	size_t i = 0;
-
-	while (i < size)
-	{
-		if (i != 0)
-			printf(", ");
-		printf("%d ", arr[i]);
-		i++;
-	}
-	printf("\n");
-}
 
 /**
  * print_list -print D.linked list element
